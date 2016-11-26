01d9dbf6e67519a14fe9ce26568a8173c0e52427
Grzegorz Kisielewicz <grzegorzk81@gmail.com>
2016-11-26 08:11:07 +0100

    Custom configuration for booki.co project - Removed defaults value of config.removeButtons and added PasteFromWord - Added h4 element to config.format_tags - Added panelbutton,colorbutton,smiley to config.PluginsExtra

--- config.js
+++ config.js
@@ -28,11 +28,15 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	config.removeButtons = 'PasteFromWord';
 
 	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+	config.format_tags = 'p;h1;h2;h3;h4;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	// Add Extra Plugins
+	config.extraPlugins = 'panelbutton,colorbutton,smiley';
+
 };
